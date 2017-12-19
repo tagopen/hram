8cab83f0160027ea8d1b59c723f4eec1b1d734c7
Artem Shcherbakov <Artem2431@gmail.com>
2017-12-19 23:01:21 +0200

    remove clndr

--- app/js/main.js
+++ app/js/main.js
@@ -55,7 +55,6 @@ var GRVE = GRVE || {};
       GRVE.showMore.init();
       GRVE.accordion.init();
       GRVE.navbar.init();
-      GRVE.calendar.init();
     }
   };
 
@@ -228,37 +227,6 @@ var GRVE = GRVE || {};
           }
         }
         ]
-      });
-    }
-  };
-
-  GRVE.calendar = {
-    init: function() {
-      // this will use clndr's default template, which you probably don't want.
-      $('#calendar').clndr();
-
-      // so instead, pass in your template as a string!
-      $('#calendar').clndr({
-        template: $('#calendar-template').html()
-      });
-
-      // there are a lot of options. the rabbit hole is deep.
-      $('#calendar').clndr({
-        template: $('#calendar-template').html(),
-        events: [
-          { date: '2013-09-09', title: 'CLNDR GitHub Page Finished', url: 'http://github.com/kylestetz/CLNDR' }
-        ],
-        clickEvents: {
-          click: function(target) {
-            console.log(target);
-          },
-          onMonthChange: function(month) {
-            console.log('you just went to ' + month.format('MMMM, YYYY'));
-          }
-        },
-        doneRendering: function() {
-          console.log('this would be a fine place to attach custom event handlers.');
-        }
       });
     }
   };
