fce2e9a082ff9f3408cad5a6db14d71dc56b32de
nucleo666 <nucleo666@gmail.com>
2017-12-22 11:26:20 +0300

    Create LiqPay.php

---
+++ app/inc/LiqPay.php
@@ -0,0 +1,111 @@
+<?php
+class LiqPay
+{
+    const CURRENCY_EUR = 'EUR';
+    const CURRENCY_USD = 'USD';
+    const CURRENCY_UAH = 'UAH';
+    const CURRENCY_RUB = 'RUB';
+    const CURRENCY_RUR = 'RUR';
+
+     private $_checkout_url = 'https://www.liqpay.ua/api/3/checkout';
+    protected $_supportedCurrencies = array(
+        self::CURRENCY_EUR,
+        self::CURRENCY_USD,
+        self::CURRENCY_UAH,
+        self::CURRENCY_RUB,
+        self::CURRENCY_RUR,
+    );
+    private $_public_key;
+    private $_private_key;
+
+
+     public function __construct($public_key, $private_key)
+    {
+        if (empty($public_key)) {
+            throw new InvalidArgumentException('public_key is empty');
+        }
+
+        if (empty($private_key)) {
+            throw new InvalidArgumentException('private_key is empty');
+        }
+
+        $this->_public_key = $public_key;
+        $this->_private_key = $private_key;
+    }
+
+     public function gen_form($params)
+    {
+        $language = 'ru';
+        if (isset($params['language']) && $params['language'] == 'en') {
+            $language = 'en';
+        }
+
+        $params    = $this->gen_params($params);
+        $data      = $this->encode_params($params);
+        $signature = $this->gen_signature($params);
+
+        return sprintf('
+            <form method="POST" action="%s" accept-charset="utf-8">
+                %s
+                %s
+                <input type="image" src="//static.liqpay.ua/buttons/p1%s.radius.png" style="display: none" name="btn_text" />
+            </form>
+            ',
+            $this->_checkout_url,
+            sprintf('<input type="hidden" name="%s" value="%s" />', 'data', $data),
+            sprintf('<input type="hidden" name="%s" value="%s" />', 'signature', $signature),
+            $language
+        );
+    }
+
+
+    public function gen_signature($params)
+    {
+        $params      = $this->gen_params($params);
+        $private_key = $this->_private_key;
+
+        $json      = $this->encode_params($params);
+        $signature = $this->str_to_sign($private_key . $json . $private_key);
+
+        return $signature;
+    }
+
+    private function gen_params($params)
+    {
+        $params['public_key'] = $this->_public_key;
+
+        if (!isset($params['version'])) {
+            throw new InvalidArgumentException('version is null');
+        }
+        if (!isset($params['amount'])) {
+            throw new InvalidArgumentException('amount is null');
+        }
+        if (!isset($params['currency'])) {
+            throw new InvalidArgumentException('currency is null');
+        }
+        if (!in_array($params['currency'], $this->_supportedCurrencies)) {
+            throw new InvalidArgumentException('currency is not supported');
+        }
+        if ($params['currency'] == self::CURRENCY_RUR) {
+            $params['currency'] = self::CURRENCY_RUB;
+        }
+        if (!isset($params['description'])) {
+            throw new InvalidArgumentException('description is null');
+        }
+
+        return $params;
+    }
+
+    private function encode_params($params)
+    {
+        return base64_encode(json_encode($params));
+    }
+
+
+    public function str_to_sign($str)
+    {
+        $signature = base64_encode(sha1($str, 1));
+
+        return $signature;
+    }
+}
